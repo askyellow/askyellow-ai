fedba5a0a7d11e49a522fc0a0441b094793360b6
askyellow <askyellow.nl@gmail.com>
2026-01-03 15:25:59 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ from fastapi import APIRouter, Request
 from passlib.context import CryptContext
 
 from passlib.context import CryptContext
-from core.time_context import build_time_context
+from core.time_context import TimeContext
 
 
 TIME_CONTEXT = TimeContext()
@@ -42,7 +42,7 @@ answer = (
     f"en het is nu {TIME_CONTEXT.time_string()}."
 )
 
-CORE_TIME_CONTEXT = build_time_context()
+TIME_CONTEXT = TimeContext()
 
 pwd_context = CryptContext(
     schemes=["bcrypt_sha256", "scrypt"],
